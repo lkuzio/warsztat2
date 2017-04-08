4c9717d4c644926d27d37a243764f05dd46147b9
lkuzio <lkuzio@gmail.com>
2017-04-08 13:09:49 +0200

    controller advice

--- src/main/java/com/pgs/controller/ErrorHandler.java
+++ src/main/java/com/pgs/controller/ErrorHandler.java
@@ -1,6 +1,10 @@
 package com.pgs.controller;
 
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 /**
  * Created by lkuzio on 08.04.17.
@@ -8,5 +12,10 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 @ControllerAdvice
 public class ErrorHandler {
 
+    @ExceptionHandler(EmptyResultDataAccessException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public void handle(EmptyResultDataAccessException ex){
+
+    }
 
 }
